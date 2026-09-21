9575bb49897b286bf1b03a141d63b5d4a5fad4eb
Angeline G. Burrell <aburrell@users.noreply.github.com>
2026-09-21 17:58:32 -0400

    ENH: added utils
    Added the sub-modules in the utils module.

---
+++ GeospaceDataManager/utils/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Utilities supporting the package classes, packages, and testing environment.
+
+"""
+
+from GeospaceDataManagement.utils._core import available_instruments  # noqa: F401
+from GeospaceDataManagement.utils._core import display_available_instruments  # noqa: F401
+from GeospaceDataManagement.utils._core import display_instrument_stats  # noqa: F401
+from GeospaceDataManagement.utils._core import generate_instrument_list  # noqa: F401
+from GeospaceDataManagement.utils._core import get_mapped_value  # noqa: F401
+from GeospaceDataManagement.utils._core import listify  # noqa: F401
+from GeospaceDataManagement.utils._core import NetworkLock  # noqa: F401
+from GeospaceDataManagement.utils._core import scale_units  # noqa: F401
+from GeospaceDataManagement.utils._core import stringify  # noqa: F401
+from GeospaceDataManagement.utils._core import update_fill_values  # noqa: F401
+from GeospaceDataManagement.utils import coords  # noqa: F401
+from GeospaceDataManagement.utils import files  # noqa: F401
+from GeospaceDataManagement.utils import io  # noqa: F401
+from GeospaceDataManagement.utils import registry  # noqa: F401
+from GeospaceDataManagement.utils import testing  # noqa: F401
+from GeospaceDataManagement.utils import time  # noqa: F401
